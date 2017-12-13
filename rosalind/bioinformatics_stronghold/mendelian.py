032f3b0983cc86fa5f5ec76863b7de4ebf2d769a
Chris Rode <chris@hooroo.com>
2017-12-14 00:07:27 +1100

    oops! lets remove the pdb traces

--- rosalind/bioinformatics_stronghold/mendelian.py
+++ rosalind/bioinformatics_stronghold/mendelian.py
@@ -1,17 +1,9 @@
-import pdb
+
 def probability_dominent(homo_dom=0, hetero=0, homo_rec=0):
   '''returns the probability of random mating producing a dominent individual'''
   total_in_population = float(homo_dom + hetero + homo_rec)
   if total_in_population < 2:
     return 0
-
-  # homo_dom_dom_alleles = 2*homo_dom
-  # homo_rec_dom_alleles = 0
-  # hetero_dom_alles = hetero
-
-  # total_dom_alles_in_population = homo_dom_dom_alleles + homo_rec_dom_alleles + hetero_dom_alles
-  # if homo_dom==1 and homo_rec==1:
-  #   pdb.set_trace()
 
   prob_homo_dom_x_homo_dom = homo_dom/total_in_population * (max(0,homo_dom-1))/(total_in_population-1)
   prob_homo_dom_x_homo_rec = homo_dom/total_in_population * homo_rec/(total_in_population-1)
