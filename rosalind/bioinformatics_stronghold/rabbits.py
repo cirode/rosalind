161a8b04e49c0cf2a5ecf01e462d8584fe5143cb
Chris Rode <chris@hooroo.com>
2017-12-17 20:21:44 +1100

    Incorporated life_expectency into the population sim

--- rosalind/bioinformatics_stronghold/rabbits.py
+++ rosalind/bioinformatics_stronghold/rabbits.py
@@ -1,10 +1,62 @@
-def breed(month, litter_size):
-  return _simulate_breeding(1, litter_size, 0, 1, month)
+def breed(months,*args,**kwargs):
+  return _simulate_breeding(end_month=months,*args,**kwargs)
 
-def _simulate_breeding(current_month, litter_size, adult_rabbit_pairs, infant_rabbit_pairs, stop_at_month):
-  total_rabbit_pairs = adult_rabbit_pairs+infant_rabbit_pairs
-  if current_month == stop_at_month:
-    return total_rabbit_pairs
-  else:
-    gestating_rabbit_pairs = adult_rabbit_pairs*litter_size
-    return _simulate_breeding(current_month+1, litter_size, total_rabbit_pairs, gestating_rabbit_pairs, stop_at_month)
+def _simulate_breeding(end_month, current_month=1, litter_size=1, rabbit_pairs=1, time_to_maturity=1,life_exp=float('inf'),_rabbits_born=None):
+  all_arguments = locals()
+  dying_rabbits = 0
+  if life_exp != float('inf') and current_month >= life_exp:
+    dying_rabbits= _rabbits_born[-life_exp]
+
+  if current_month == end_month:
+    return rabbit_pairs
+  if _rabbits_born is None:
+    _rabbits_born = [rabbit_pairs]
+
+  infant_rabbits = sum(_rabbits_born[-(time_to_maturity):])
+  adult_rabbits = rabbit_pairs - infant_rabbits
+  
+  new_rabbits = adult_rabbits*litter_size
+  _rabbits_born.append(new_rabbits)
+  rabbit_pairs += new_rabbits
+
+  all_arguments.update({ \
+    '_rabbits_born': _rabbits_born, \
+    'current_month': current_month+1, \
+    'rabbit_pairs':rabbit_pairs - dying_rabbits
+  })
+  return _simulate_breeding(**all_arguments)
+
+
+# class Population():
+
+#   def __init__(self, litter_size=1,rabbit_pairs=1):
+#     self.rabbit_pairs=rabbit_pairs
+#     self.life_exp = 1
+#     self._litter_size = litter_size
+#     self.month = 1
+#     self._time_to_maturity = 1
+#     self._rabbits_born = [rabbit_pairs]
+    
+
+#   def simulate_months(self,months):
+#     for _ in xrange(months):
+#       # self.dying
+#       self.month += 1
+#       self.birthing()
+#       print(self.month)
+#       print(self.rabbit_pairs)
+#       print(self._rabbits_born)
+
+#   def birthing(self):
+#     new_rabbits = self._adult_rabbits() * self._litter_size
+#     self._rabbits_born.append(new_rabbits)
+#     self.rabbit_pairs += new_rabbits
+
+#   def _adult_rabbits(self):
+#     print("INFants:{}".format(self._infant_rabbits()))
+#     return self.rabbit_pairs - self._infant_rabbits()
+
+#   def _infant_rabbits(self):
+#     if self.month <= (self._time_to_maturity+1):
+#       return self.rabbit_pairs
+#     return self.rabbit_pairs - sum(self._rabbits_born[:-(self._time_to_maturity+1)])
--- specs/bioinformatics_stronghold/rabbits_spec.py
+++ specs/bioinformatics_stronghold/rabbits_spec.py
@@ -3,7 +3,11 @@ from rosalind.bioinformatics_stronghold.rabbits import *
 
 
 with description('breed'):
-  with context('it is given the number of offspring per generation and the number of months to simulate'):
-    with it('returns the number of pairs that will be present in the nth month'):
-      expect(breed(5,3)).to(equal(19))
+  with context('it is given the number of offspring per generation and the number of months to simulate (fib)'):
+    with it('returns the number of pairs that will be present in the nth month (assuming a life_exp of INF)'):
+      expect(breed(litter_size=3,months=5)).to(equal(19))
+
+  with context('it is given the number of months each generation lives and the number of months to simulate (fibd)'):
+    with it('returns the number of pairs that will be present in the nth month (assuming a litter_size of 1)'):
+      expect(breed(life_exp=3,months=6)).to(equal(4))
 
